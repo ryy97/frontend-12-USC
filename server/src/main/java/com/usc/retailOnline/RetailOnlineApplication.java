a9476b45e72dee0d04eaa99a45cfe3cd10405818
ryy97 <yran4@asu.edu>
2022-10-24 19:50:08 -0700

    add server

---
+++ server/src/main/java/com/usc/retailOnline/RetailOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.usc.retailOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailOnlineApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/usc/retailOnline/RetailOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usc.retailOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
